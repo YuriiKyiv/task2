cb8a6c5c4ae5f0d2184b40e4926a4f2fe4865ad1
YURII <yurii@YURIIs-iMac.local>
2015-02-11 12:21:10 +0200

    add the second macros

--- task2/main.c
+++ task2/main.c
@@ -8,15 +8,23 @@
 
 #include <stdio.h>
 
-#define genHere( type, spec ) \
-void MyOutput_##type(type value ) { \
+#define genHere(type, spec) \
+void MyOutput_##type(type value) { \
     printf(spec, value); \
 } \
+
+#define MyOutputValue(type, value) \
+MyOutput_##type(value); \
+
+genHere( int, "%d" );
+genHere( char, "%c" );
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    MyOutput_int(12);
+    MyOutput_char("a");
+    MyOutputValue(int, 15)
+    MyOutputValue(char,"b")
     return 0;
 }
-
-genHere( int, "%d" );
